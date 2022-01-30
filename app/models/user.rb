b25c07990fdac0e2abd8b1e76e065f6d992ca4c5
Enow Mbi <benowmbi@yahoo.com>
2022-01-30 14:54:25 +0100

    Add followers relationship to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,20 @@
 class User < ApplicationRecord
-  has_many :following_relationships, foreign_key: :follower_id
+  has_many :following_relationships, foreign_key: :follower_id, dependent: :destroy
   has_many :followed_users, through: :following_relationships
 
+  has_many :follower_relationships, foreign_key: :followed_user_id, class_name: "FollowingRelationship", dependent: :destroy
+  has_many :followers, through: :follower_relationships
 
-  def follow user
+
+  def follow(user)
     followed_users << user
   end
 
-  def following? user
+  def following?(user)
     followed_user_ids.include? user.id
+  end
+
+  def unfollow(user)
+    followed_users.delete(user)
   end
 end
