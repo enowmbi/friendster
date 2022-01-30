1d71a45c46acedae743cea3f3dd9eaf499a54d59
Enow Mbi <benowmbi@yahoo.com>
2022-01-30 14:27:02 +0100

    Add ability to add user to followed_users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ApplicationRecord
+  has_many :following_relationships, foreign_key: :follower_id
+  has_many :followed_users, through: :following_relationships
+
+
+  def follow user
+    followed_users << user
+  end
+
+  def following? user
+    followed_user_ids.include? user.id
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_29_094417) do
+ActiveRecord::Schema.define(version: 2022_01_30_123314) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "following_relationships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "friendships", force: :cascade do |t|
     t.integer "following_id"
